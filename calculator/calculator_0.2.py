612a8e7c5d7fb4785d179fb2166dbd3565a9ab50
Lukasz Zurek <lukasz4ever@gmail.com>
2016-07-04 18:31:45 +0200

    04072016_2

--- calculator/calculator_0.2.py
+++ calculator/calculator_0.2.py
@@ -1,22 +1,25 @@
 # Program calculator. The calculator can calculate basic operations like +, -, /, * with two positive numbers.
-# Please separate numbers and operators with withe space.
+# You don't need separate numbers and operators with withe space.
 start = input("Co chcesz obliczyć?\n")
 while start != "":
-    part = start.split()
-    if part[1] == "+":
-        score = int(part[0]) + int(part[2])
+    if "+" in start:
+        part = start.split("+")
+        score = int(part[0]) + int(part[1])
         print(score)
-    elif part[1] == "-":
-        score = int(part[0]) - int(part[2])
+    elif "-" in start:
+        part = start.split("-")
+        score = int(part[0]) - int(part[1])
         print(score)
-    elif part[1] == "/":
-        if int(part[2]) == 0:
+    elif "/" in start:
+        part = start.split("/")
+        if int(part[1]) == 0:
             print("Dzielenie przez 0 nie jest możliwe")
         else:
-            score = int(part[0]) / int(part[2])
+            score = int(part[0]) / int(part[1])
             print(score)
-    elif part[1] == "*":
-        score = int(part[0]) * int(part[2])
+    elif "*" in start:
+        part = start.split("*")
+        score = int(part[0]) * int(part[1])
         print(score)
     start = input("Co chcesz obliczyć?\n")
 print("Koniec")
